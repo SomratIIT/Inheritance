1ac846bd549f7e984e4c4e1fb456b204af6f9ecb
SomratIIT <101197949+SomratIIT@users.noreply.github.com>
2024-02-02 12:38:59 +0600

    Add files via upload

---
+++ EncapTest1.java
@@ -0,0 +1,16 @@
+package study1;
+
+public class EncapTest1 {
+public static void main(String[] args)
+{
+	Person p1 = new Person();
+	
+	p1.setName("Somrat");
+	System.out.println(p1.getName());
+	
+	
+	p1.setAge(25);
+	System.out.println(p1.getAge());
+	
+}
+}
